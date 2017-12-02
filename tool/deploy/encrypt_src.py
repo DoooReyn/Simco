a0d3b24a269dbf8c0f76cea3037e5bd576c02a5a
DoooReyn <jl88744653@gmail.com>
2017-12-03 05:31:26 +0800

    enctrypt for android and ios

--- tool/deploy/encrypt_src.py
+++ tool/deploy/encrypt_src.py
@@ -5,11 +5,26 @@ import os
 
 SOURCE_DIR = '../../src/'
 OUT_DIR = './package/Update/src/'
+KEY  = 'reyn@simco@07171991@KEY'
+SIGN = '!19917170!comis!nyer@SIGN'
 
-def encrypt_lua_file(src, out):
-    cmd = "cocos luacompile -s %s -d %s -e -k reyn@simco@07171991@KEY -b !19917170!comis!nyer@SIGN" % (src, OUT_DIR)
+def encrypt_lua_file_for_android(src, out, key, sign):
+    cmd = "cocos luacompile -s %s -d %s -e -k %s -b %s" % (src, OUT_DIR, key, sign)
+    os.system(cmd)
+
+def encrypt_lua_file_for_ios(src, out, key, sign):
+    cmd = "cocos luacompile -s %s -d %s -e -k %s -b %s --disable-compile" % (src, OUT_DIR, key, sign)
     os.system(cmd)
 
 
 if __name__ == '__main__':
-    encrypt_lua_file(SOURCE_DIR, OUT_DIR)
+    import sys
+    if len(sys.argv) == 1:
+        print 'need target platform : android/ios'
+    else:
+        if sys.argv[1] == 'android':
+            encrypt_lua_file_for_android(SOURCE_DIR, OUT_DIR, KEY, SIGN)
+        elif sys.argv[1] == 'ios':
+            encrypt_lua_file_for_ios(SOURCE_DIR, OUT_DIR, KEY, SIGN)
+        else:
+            print 'need target platform : android/ios'
