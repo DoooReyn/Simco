135e4ad5c1b8ef8dd7a1663fc3e2b4b56ed61f51
DoooReyn <jl88744653@gmail.com>
2017-12-03 02:51:30 +0800

    move version files to directory version

---
+++ version/assets.lua
@@ -0,0 +1,306 @@
+return {
+	['main.lua'] = {
+		md5 = '5370b0ec7bbc66782019784f4023f340',
+		size = 244
+	},
+	['config.lua'] = {
+		md5 = '45f11be10ba1670e7ff9d960d9ce87b8',
+		size = 654
+	},
+	['app.app.lua'] = {
+		md5 = '68e72fa180b316fa3d85af47dcb8f2b8',
+		size = 758
+	},
+	['app.util.network.constant.lua'] = {
+		md5 = 'a642cc43fdba3edb106f280d5b8965fc',
+		size = 348
+	},
+	['app.util.network.hotupdate.lua'] = {
+		md5 = 'd41d8cd98f00b204e9800998ecf8427e',
+		size = 0
+	},
+	['app.util.network.download.lua'] = {
+		md5 = '3241fe73f228ee18701c87e5e0971cf5',
+		size = 4510
+	},
+	['app.util.network.subpackage.lua'] = {
+		md5 = 'd41d8cd98f00b204e9800998ecf8427e',
+		size = 0
+	},
+	['app.util.hotupdate.hotupdate.lua'] = {
+		md5 = 'f6b4cf89b10ee587862ce2031c5f7290',
+		size = 4552
+	},
+	['app.util.env.const.lua'] = {
+		md5 = 'a9a48d38a7c4fd4f8de35f8a0e17036d',
+		size = 1117
+	},
+	['app.util.env.env.lua'] = {
+		md5 = 'eaa97f95a1124d0b55eb8e9e239e0783',
+		size = 1485
+	},
+	['app.util.log.log.lua'] = {
+		md5 = '90552cb2cd7208ab06e32412b0d0ef4d',
+		size = 802
+	},
+	['app.views.MainScene.lua'] = {
+		md5 = 'c1e2d15cf9c7dbea0494dbf49da89ae3',
+		size = 392
+	},
+	['cocos.init.lua'] = {
+		md5 = '6e1183ff508b33b16c954a1ec7d88102',
+		size = 3207
+	},
+	['cocos.ui.experimentalUIConstants.lua'] = {
+		md5 = '2987cda864012842127696fb0ede80f6',
+		size = 130
+	},
+	['cocos.ui.GuiConstants.lua'] = {
+		md5 = 'f92735d93efdf23b460abd7e6ad01893',
+		size = 3404
+	},
+	['cocos.ui.DeprecatedUIFunc.lua'] = {
+		md5 = '75eefff802a9a35c6bed4a8c22282e11',
+		size = 9831
+	},
+	['cocos.ui.DeprecatedUIEnum.lua'] = {
+		md5 = '10d416489ec271eb6c4f7c1bda08e03e',
+		size = 5126
+	},
+	['cocos.3d.3dConstants.lua'] = {
+		md5 = 'e825060c9abf336a04ca5fdc2619d9ad',
+		size = 198
+	},
+	['cocos.cocosbuilder.DeprecatedCocosBuilderClass.lua'] = {
+		md5 = '66d1418e6996754dcc6def1ecc0f2bb9',
+		size = 1176
+	},
+	['cocos.cocosbuilder.CCBReaderLoad.lua'] = {
+		md5 = 'bd8033dcac7a2ff66ffc28ecf1bf481a',
+		size = 5574
+	},
+	['cocos.extension.DeprecatedExtensionEnum.lua'] = {
+		md5 = 'c70b3195c37a800cdf39b4d632ddb822',
+		size = 1410
+	},
+	['cocos.extension.DeprecatedExtensionClass.lua'] = {
+		md5 = '4680cbe5597845050f979315bf9b288b',
+		size = 4838
+	},
+	['cocos.extension.DeprecatedExtensionFunc.lua'] = {
+		md5 = '767d15a232e5b69b4f4bd30329d31ccd',
+		size = 1351
+	},
+	['cocos.extension.ExtensionConstants.lua'] = {
+		md5 = '2e493600ea66630fe265adb019ba4292',
+		size = 2000
+	},
+	['cocos.spine.SpineConstants.lua'] = {
+		md5 = 'ab87729cbab5c1f32f950760716dc751',
+		size = 212
+	},
+	['cocos.framework.init.lua'] = {
+		md5 = 'c09a77b80b6a8552547b06495f42443d',
+		size = 2706
+	},
+	['cocos.framework.package_support.lua'] = {
+		md5 = '9af20f65abc3c7a7dc866587528bd2e9',
+		size = 4015
+	},
+	['cocos.framework.display.lua'] = {
+		md5 = '7e51563c254320b7313c962fd250cbc2',
+		size = 21352
+	},
+	['cocos.framework.device.lua'] = {
+		md5 = '5712f4b78af552909708c88c6e30c767',
+		size = 3851
+	},
+	['cocos.framework.audio.lua'] = {
+		md5 = '1688cecc51cd8cf2b2df32c3587dad74',
+		size = 5657
+	},
+	['cocos.framework.transition.lua'] = {
+		md5 = '5667b9e34babbb60c6dde79cda623cf5',
+		size = 7522
+	},
+	['cocos.framework.extends.SpriteEx.lua'] = {
+		md5 = '0db1c7fa60f27ea680d6ac31c4165005',
+		size = 2156
+	},
+	['cocos.framework.extends.UISlider.lua'] = {
+		md5 = 'f82bcaf11beb658da0a7fdc5a3c04aa8',
+		size = 1402
+	},
+	['cocos.framework.extends.UITextField.lua'] = {
+		md5 = '0d05ab78e29180bbdf943121fbc011e1',
+		size = 1635
+	},
+	['cocos.framework.extends.UIScrollView.lua'] = {
+		md5 = 'a7425fa31d52266cd757169fa2d44e35',
+		size = 2210
+	},
+	['cocos.framework.extends.UICheckBox.lua'] = {
+		md5 = '613c3b42c1a3a053f93f8a9708288664',
+		size = 1446
+	},
+	['cocos.framework.extends.UIEditBox.lua'] = {
+		md5 = 'fd67393654fba0d9b2fa39223e32260c',
+		size = 1452
+	},
+	['cocos.framework.extends.UIListView.lua'] = {
+		md5 = 'dd18362d2049ab105a2f9ddd47e5bfd9',
+		size = 2526
+	},
+	['cocos.framework.extends.NodeEx.lua'] = {
+		md5 = '83fc8bc4c2dfe490d4f7d6a6304a65e5',
+		size = 5171
+	},
+	['cocos.framework.extends.LayerEx.lua'] = {
+		md5 = 'f137a7632ade189c20048e5df9731eb1',
+		size = 2622
+	},
+	['cocos.framework.extends.UIWidget.lua'] = {
+		md5 = '4c09920e460819a58eafb77664fa6cfd',
+		size = 1572
+	},
+	['cocos.framework.extends.MenuEx.lua'] = {
+		md5 = '28d5d077848078eace868d55c724318e',
+		size = 1238
+	},
+	['cocos.framework.extends.UIPageView.lua'] = {
+		md5 = 'c6027914fb76fe350af4e3ab081e67d7',
+		size = 1394
+	},
+	['cocos.framework.components.event.lua'] = {
+		md5 = 'ce90b474524bb36c82833acd3d23a741',
+		size = 4752
+	},
+	['cocos.network.DeprecatedNetworkEnum.lua'] = {
+		md5 = '01ed31489d348143eec7d398667da333',
+		size = 522
+	},
+	['cocos.network.DeprecatedNetworkFunc.lua'] = {
+		md5 = '31cc44f404de667d3961b37932a56720',
+		size = 1123
+	},
+	['cocos.network.DeprecatedNetworkClass.lua'] = {
+		md5 = '47fab20d513b00b62ade2cac15b7665c',
+		size = 564
+	},
+	['cocos.network.NetworkConstants.lua'] = {
+		md5 = '037a9d6ed7ae01a1336215c3f05c7b14',
+		size = 511
+	},
+	['cocos.controller.ControllerConstants.lua'] = {
+		md5 = 'ec71a0f02b11769725420f79afdecf11',
+		size = 727
+	},
+	['cocos.cocos2d.functions.lua'] = {
+		md5 = '44ec84f6aaa5a8d6a2274f2fd54063ff',
+		size = 17169
+	},
+	['cocos.cocos2d.json.lua'] = {
+		md5 = 'af25b930d1202aea87039ddf380488b2',
+		size = 15353
+	},
+	['cocos.cocos2d.DeprecatedOpenglEnum.lua'] = {
+		md5 = '686a2e1cd0366cb3e98abd85e99139db',
+		size = 11934
+	},
+	['cocos.cocos2d.Opengl.lua'] = {
+		md5 = 'fcfa59dc2f11ce8b34dd5433b2cba7b3',
+		size = 6853
+	},
+	['cocos.cocos2d.DeprecatedCocos2dEnum.lua'] = {
+		md5 = 'c1a66a0d053f5699e3fe29b4348e247f',
+		size = 16871
+	},
+	['cocos.cocos2d.Cocos2dConstants.lua'] = {
+		md5 = '18a3378d7f0554da2b8125241430e943',
+		size = 16670
+	},
+	['cocos.cocos2d.luaj.lua'] = {
+		md5 = '6a8d809ce792003de4e9ef2b19d3a69c',
+		size = 927
+	},
+	['cocos.cocos2d.luaoc.lua'] = {
+		md5 = '7dac884047788e8a67ee74fb502de398',
+		size = 879
+	},
+	['cocos.cocos2d.OpenglConstants.lua'] = {
+		md5 = '5bd7bff274280d638a92705333c5ab47',
+		size = 27127
+	},
+	['cocos.cocos2d.DeprecatedCocos2dClass.lua'] = {
+		md5 = '025676dfdd3f9c19b6024de9aca9a1d6',
+		size = 60737
+	},
+	['cocos.cocos2d.DrawPrimitives.lua'] = {
+		md5 = '52ed23a28af37410a9d2518a6d4f6557',
+		size = 12024
+	},
+	['cocos.cocos2d.DeprecatedCocos2dFunc.lua'] = {
+		md5 = 'bdcc9a4159e006f2eb050b6928358c54',
+		size = 39472
+	},
+	['cocos.cocos2d.bitExtend.lua'] = {
+		md5 = 'd97acd155084fd08cd5945e4ad7dbad6',
+		size = 1590
+	},
+	['cocos.cocos2d.deprecated.lua'] = {
+		md5 = '117f054ef632396cc602f54850bac49d',
+		size = 491
+	},
+	['cocos.cocos2d.Cocos2d.lua'] = {
+		md5 = 'd3e3e5e63bab2b8ce0a1b5d960c11aeb',
+		size = 13384
+	},
+	['cocos.cocosdenshion.DeprecatedCocosDenshionFunc.lua'] = {
+		md5 = 'b2a937b827f4534fe93fedb18c3afc60',
+		size = 912
+	},
+	['cocos.cocosdenshion.AudioEngine.lua'] = {
+		md5 = 'e5a781ae884dffaad788bb9d3469c2f9',
+		size = 2666
+	},
+	['cocos.cocosdenshion.DeprecatedCocosDenshionClass.lua'] = {
+		md5 = '394e23992d05bd21a80f6480dc7aa689',
+		size = 661
+	},
+	['cocos.physics3d.physics3d-constants.lua'] = {
+		md5 = '445368a800dddc8f06e2ec69dd735e1a',
+		size = 261
+	},
+	['cocos.cocostudio.DeprecatedCocoStudioFunc.lua'] = {
+		md5 = '4b9fd27a60881171e466f834b57a6664',
+		size = 3456
+	},
+	['cocos.cocostudio.StudioConstants.lua'] = {
+		md5 = 'cf15865a5ca60bcdbe29ab2364a24824',
+		size = 208
+	},
+	['cocos.cocostudio.DeprecatedCocoStudioClass.lua'] = {
+		md5 = '15f0e49b346f0026b38ea4294de278d3',
+		size = 10745
+	},
+	['cocos.cocostudio.CocoStudio.lua'] = {
+		md5 = 'aa153ca7cb50004a8da99dd48b7a4b78',
+		size = 9435
+	},
+	['packages.mvc.AppBase.lua'] = {
+		md5 = '6d6cbd32172bd49e71461168cc1bd5da',
+		size = 1853
+	},
+	['packages.mvc.init.lua'] = {
+		md5 = '6b9173481a1300c5e737ad5885ebef00',
+		size = 94
+	},
+	['packages.mvc.ViewBase.lua'] = {
+		md5 = '4499d9879d448bf7c35babbff918e8da',
+		size = 1888
+	},
+	['HelloWorld.png'] = {
+		md5 = '55ea4e952bf080f300379ec26723598b',
+		size = 37864
+	},
+}
---
+++ version/version.lua
@@ -0,0 +1,5 @@
+return {
+    code   = 1
+    build  = '1.1.0',
+    update = true
+}
